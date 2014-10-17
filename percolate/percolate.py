8e8f40ff713c2ab53716a9307f4195dfd4f8a442
Andreas Sorge <as@asorge.de>
2014-10-17 12:54:47 +0200

    Implement percolate.sample_states

--- percolate/percolate.py
+++ percolate/percolate.py
@@ -60,14 +60,13 @@ def sample_states(graph, spanning_cluster=True, model='bond'):
     ret['has_spanning_cluster'] : bool
         ``True`` if there is a spanning cluster, ``False`` otherwise.
 
-    ret['max_cluster_size'] : float
-        Size of the largest cluster, as a fraction of the total number of
-        sites.
+    ret['max_cluster_size'] : int
+        Size of the largest cluster (absolute number of sites)
 
-    ret['moments'] : 1-D :py:class:`numpy.ndarray`
+    ret['moments'] : 1-D :py:class:`numpy.ndarray` of int
         Array of size ``5``.
-        The ``k``-th entry is the ``k``-th raw moment of the cluster size
-        distribution, with ``k`` ranging from ``0`` to ``4``.
+        The ``k``-th entry is the ``k``-th raw moment of the (absolute) cluster
+        size distribution, with ``k`` ranging from ``0`` to ``4``.
 
     Raises
     ------
@@ -88,17 +87,17 @@ def sample_states(graph, spanning_cluster=True, model='bond'):
     Spanning cluster
 
         In order to detect a spanning cluster, `graph` needs to contain
-        auxiliary nodes, cf. Reference [1]_, Figure 6.
-        The auxiliary nodes have the ``'span'`` `attribute
+        auxiliary nodes and edges, cf. Reference [1]_, Figure 6.
+        The auxiliary nodes and edges have the ``'span'`` `attribute
         <http://networkx.github.io/documentation/latest/tutorial/tutorial.html#node-attributes>`_.
         The value is either ``0`` or ``1``, distinguishing the two sides of the
         graph to span.
 
     Raw moments of the cluster size distribution
 
-        The :math:`k`-th raw moment of the cluster size distribution is
-        :math:`\sum_s' s^k n_s`, where :math:`s` is the cluster size and
-        :math:`n_s` is the number of clusters of size :math:`s` per site. [2]_
+        The :math:`k`-th raw moment of the (absolute) cluster size distribution
+        is :math:`\sum_s' s^k N_s`, where :math:`s` is the cluster size and
+        :math:`N_s` is the number of clusters of size :math:`s`. [2]_
         The primed sum :math:`\sum'` signifies that the largest cluster is
         excluded from the sum. [3]_
 
@@ -122,6 +121,8 @@ def sample_states(graph, spanning_cluster=True, model='bond'):
                 'Spanning cluster is to be detected, but auxiliary nodes '
                 'of less or more than 2 types (sides) given.'
             )
+
+        auxiliary_edge_attributes = nx.get_edge_attributes(graph, 'span')
 
     # get subgraph on which percolation is to take place (strip off the
     # auxiliary nodes)
@@ -148,13 +149,90 @@ def sample_states(graph, spanning_cluster=True, model='bond'):
     ret = dict()
 
     ret['n'] = 0
-    ret['max_cluster_size'] = 1 / num_nodes
-    ret['moments'] = np.ones(5) * (num_nodes - 1) / num_nodes
+    ret['max_cluster_size'] = 1
+    ret['moments'] = np.ones(5, dtype=int) * (num_nodes - 1)
 
     if spanning_cluster:
         ret['has_spanning_cluster'] = False
 
     yield ret
 
-    # loop over all edges (n == 1..M)
+    # permute edges
+    perm_edges = np.random.permutation(num_edges)
 
+    # set up disjoint set (union-find) data structure
+    ds = nx.utils.union_find.UnionFind()
+    if spanning_cluster:
+        ds_spanning = nx.utils.union_find.UnionFind()
+
+        # merge all auxiliary nodes for each side
+        side_roots = dict()
+        for side in spanning_sides:
+            nodes = [
+                node for (node, node_side) in auxiliary_node_attributes.items()
+                if node_side is side
+            ]
+            ds_spanning.union(*nodes)
+            side_roots[side] = ds_spanning[nodes[0]]
+
+        for (edge, edge_side) in auxiliary_edge_attributes.items():
+            ds_spanning.union(side_roots[edge_side], *edge)
+
+        side_roots = [ ds_spanning[side_root] for side_root in side_roots.values() ]
+
+
+    # get first node
+    max_cluster_root = next(perc_graph.nodes_iter())
+
+    # loop over all edges (n == 1..M)
+    for n in range(num_edges):
+        ret['n'] = n + 1
+
+        # draw new edge from permutation
+        edge_index = perm_edges[n]
+        edge = perc_edges[edge_index]
+
+        # find roots and weights
+        roots = [ ds[node] for node in edge ]
+        weights = [ ds.weights[root] for root in roots ]
+
+        if roots[0] is not roots[1]:
+            # not same cluster: union!
+            ds.union(*roots)
+            if spanning_cluster:
+                ds_spanning.union(*roots)
+
+                ret['has_spanning_cluster'] = (
+                    ds_spanning[side_roots[0]] == ds_spanning[side_roots[1]]
+                )
+
+            # find new root and weight
+            root = ds[edge[0]]
+            weight = ds.weights[root]
+
+            # moments and maximum cluster size
+
+            # deduct the previous sub-maximum clusters from moments
+            for i in [0, 1]:
+                if roots[i] is max_cluster_root:
+                    continue
+                ret['moments'] -= weights[i] ** np.arange(5)
+
+            if max_cluster_root in roots:
+                # merged with maximum cluster
+                max_cluster_root = root
+                ret['max_cluster_size'] = weight
+            else:
+                # merged previously sub-maximum clusters
+                if ret['max_cluster_size'] >= weight:
+                    # previously largest cluster remains largest cluster
+                    # add merged cluster to moments
+                    ret['moments'] += weight ** np.arange(5)
+                else:
+                    # merged cluster overtook previously largest cluster
+                    # add previously largest cluster to moments
+                    max_cluster_root = root
+                    ret['moments'] += ret['max_cluster_size'] ** np.arange(5)
+                    ret['max_cluster_size'] = weight
+
+        yield ret
--- tests/test_percolate.py
+++ tests/test_percolate.py
@@ -49,13 +49,24 @@ def empty_graph():
 def grid_3x3_graph(request):
     import networkx
 
+    ret = networkx.Graph()
+    ret.add_nodes_from(range(9))
+    ret.add_edges_from([
+        (i, i + j) for i in [1, 4, 7] for j in [-1, 1]
+    ])
+    ret.add_edges_from([
+        (i, i + j) for i in [3, 4, 5] for j in [-3, 3]
+    ])
+
     if request.param:
-        ret = networkx.grid_2d_graph(5, 3)
-        for i in [0, 4]:
-            for j in range(3):
-                ret.node[(i, j)]['span'] = i
-    else:
-        ret = networkx.grid_2d_graph(3, 3)
+        ret.add_nodes_from(range(9, 12), span=0)
+        ret.add_nodes_from(range(12, 15), span=1)
+        ret.add_edges_from(
+            [(0, 9), (3, 10), (6, 11)], span=0
+        )
+        ret.add_edges_from(
+            [(2, 12), (5, 13), (8, 14)], span=1
+        )
 
     ret.graph['span'] = request.param
     return ret
@@ -85,12 +96,95 @@ def test_initial_iteration(grid_3x3_graph):
     ))
 
     assert ret['n'] == 0
-    np.testing.assert_allclose(ret['max_cluster_size'], 1 / 9)
+    assert ret['max_cluster_size'] == 1
     assert np.array_equal(
-        ret['moments'], np.ones(5) * 8 / 9
+        ret['moments'], np.ones(5, dtype=int) * 8
     )
 
     assert ('has_spanning_cluster' in ret) == spanning_cluster
 
     if spanning_cluster:
         assert not ret['has_spanning_cluster']
+
+
+@pytest.fixture
+def state_it(grid_3x3_graph):
+    spanning_cluster = grid_3x3_graph.graph['span']
+    ret = percolate.sample_states(
+        grid_3x3_graph, spanning_cluster=spanning_cluster
+    )
+    return ret, spanning_cluster
+
+
+def test_number_of_iterations(state_it):
+
+    n = 0
+
+    for state in state_it[0]:
+        assert state['n'] == n
+        n += 1
+
+    assert state['n'] == 12
+
+
+# TEST CASE:
+# 3 x 3 grid with edge indices
+#
+#  . 10  . 11  .
+#  6     8     9
+#  .  5  .  7  .
+#  1     3     4
+#  .  0  .  2  .
+#
+# The edge permutation is
+# [10, 9, 0, 8, 5, 2, 1, 11, 4, 7, 3, 6]
+
+def test_max_cluster_size(state_it):
+
+    np.random.seed(42)
+
+    max_cluster_sizes = [
+        1, 2, 2, 2, 3, 4, 4, 7, 9, 9, 9, 9, 9
+    ]
+
+    for state in state_it[0]:
+        assert state['max_cluster_size'] == max_cluster_sizes[state['n']]
+
+
+def test_moments(state_it):
+
+    np.random.seed(42)
+
+    moments = np.array([
+        [8, 8, 8, 8, 8],     # n == 0
+        [7, 7, 7, 7, 7],     # n == 1
+        [6, 7, 9, 13, 21],
+        [5, 7, 11, 19, 35],
+        [4, 6, 10, 18, 34],
+        [3, 5, 9, 17, 33],
+        [2, 5, 13, 35, 97],
+        [1, 2, 4, 8, 16],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ])
+
+    for state in state_it[0]:
+        assert np.array_equal(moments[state['n']], state['moments'])
+
+
+def test_spanning(state_it):
+
+    if not state_it[1]:
+        return
+
+    np.random.seed(42)
+
+    has_spanning_cluster = 6 * [False, ] + 7 * [True, ]
+
+    for state in state_it[0]:
+        assert (
+            state['has_spanning_cluster'] == has_spanning_cluster[state['n']]
+        )
