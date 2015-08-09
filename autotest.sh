6bc973545039f34ae72e62344a0b503c5f0bb9b2
Andreas Sorge <as@asorge.de>
2015-08-09 20:23:08 +0200

    Configure local continuous testing to use pytest-pep8

--- autotest.sh
+++ autotest.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+py.test
 while inotifywait -qq -r -e modify -e create -e move -e delete \
        --exclude '\.sw.?$' percolate
 do
 	clear
-	py.test --cov=percolate percolate
+	py.test
 	sleep 1
 done 
