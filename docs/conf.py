0fadc7e5668b18fa2b510252e49462388be97a7a
Andreas Sorge <as@asorge.de>
2015-08-13 17:39:53 +0200

    DOC Fix future

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return Mock()
 
-MOCK_MODULES = ['numpy', 'scipy', 'scipy.stats', 'matplotlib', 'networkx', 'future']
+MOCK_MODULES = ['numpy', 'scipy', 'scipy.stats', 'matplotlib', 'networkx', ]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 import alabaster
